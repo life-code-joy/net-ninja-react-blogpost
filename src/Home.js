0013901ee3a95566d4e24c531c568f9ea22b4a72
darren <darren__c@hotmail.com>
2021-01-01 20:39:44 +0900

    tut7-event listeners

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,18 @@
 const Home = () => {
+  const handleClick = (e) => {
+    console.log("hello ", e);
+  };
+
+  const handleClickAgain = (name, e) => {
+    console.log("hello " + name, e.target);
+  };
   return (
     <div className="home">
       <h2>Home Page</h2>
+      <button onClick={handleClick}>Click Me</button>
+      <button onClick={(e) => handleClickAgain("mario", e)}>
+        Click me again
+      </button>
     </div>
   );
 };
